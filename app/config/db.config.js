fb3e5566f9fe6efdb807d9f62a7101e687f209b0
saithihaaung1997 <nandarmyintaung1997@gmail.com>
2021-02-05 10:40:04 +0630

    webAppServer

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
-  DB: "testing",
+  DB: "webdb",
   dialect: "mysql",
   pool: {
     max: 5,
--- server.js
+++ server.js
@@ -16,15 +16,19 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-db.sequelize.sync();
+const userDetail = require("./app/models/userDetails");
+userDetail.sequelize.sync();
+
+const superAdmin = require("./app/models/superAdmin");
+superAdmin.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to application." });
 });
 
-require("./app/routes/userdetails.routes")(app);
+require("./app/routes/userdetails/userdetails.routes")(app);
+require("./app/routes/superAdmin/superAdmin.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
